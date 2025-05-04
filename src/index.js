191711874bef56b4f9df57f60855d0076582bfaf
lorenz <lore@lore.com>
2025-05-04 20:18:46 +0200

    fix stampa 2 volte mostraTutti

--- src/index.js
+++ src/index.js
@@ -10,24 +10,33 @@ const prompt = p();
 
 let promemoria = [];
 let scelta = 0;
+let m = false;
 
-do{
-    mostraPrimi(promemoria);
-    console.log("Cosa vuoi fare?\n1)Aggiungere un promemoria\n2)Cancellare un promemoria\n3)Visualizzare tutti i promemoria\n4)Visualizzare/Modificare dettagli promemoria\n0)Esci");
-    scelta = parseInt(prompt());
-    switch(scelta){
-        case 1:
-            aggiungi(promemoria, prompt);
-            break;
-        case 2:
-            cancella(promemoria, prompt);
-            break;
-        case 3:
-            mostraTutti(promemoria);
-            break;
-        case 0:
-            break;
-        default:
-            console.log("Scelta non valida");
-    }
-}while(scelta != 0);
+function main(){
+    do{
+        if(!m)
+            mostraPrimi(promemoria);
+        else
+            m = !m;
+        console.log("Cosa vuoi fare?\n1)Aggiungere un promemoria\n2)Cancellare un promemoria\n3)Visualizzare tutti i promemoria\n4)Visualizzare/Modificare dettagli promemoria\n0)Esci");
+        scelta = parseInt(prompt());
+        switch(scelta){
+            case 1:
+                aggiungi(promemoria, prompt);
+                break;
+            case 2:
+                cancella(promemoria, prompt);
+                break;
+            case 3:
+                mostraTutti(promemoria);
+                m = true;
+                break;
+            case 0:
+                break;
+            default:
+                console.log("Scelta non valida");
+        }
+    }while(scelta != 0);
+}
+
+main();
